2869be77f4a941195bf7216e7bfa23aab7d0a017
= <stamno@pm.me>
2025-03-03 21:53:25 -0700

    added json Post for user submissons

--- src/Main.elm
+++ src/Main.elm
@@ -6,6 +6,9 @@ import Html.Attributes exposing (placeholder, type_, value, class)
 import Html.Events exposing (onClick, onInput)
 import Time exposing (Posix, posixToMillis)
 import Task
+import Json.Encode as Encode
+import Http
+import Json.Decode as Decode
 
 -- MODEL
 
@@ -20,6 +23,14 @@ type alias Model =
     }
 
 type alias Submission =
+    { name : String
+    , link : String
+    , notes : String
+    , date : String
+    , grade : String
+    }
+
+type alias SubmissionJson =
     { name : String
     , link : String
     , notes : String
@@ -52,6 +63,9 @@ type Msg
     | UpdatePassword String
     | CheckPassword
     | UpdateGrade String String
+    | FetchSubmissions
+    | FetchedSubmissions (Result Http.Error (List Submission))
+    | SubmissionSaved (Result Http.Error ())
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -69,8 +83,12 @@ update msg model =
             let
                 newSubmission =
                     { name = model.name, link = model.link, notes = model.notes, date = model.date, grade = "" }
+                submissionJson =
+                    encodeSubmission newSubmission
             in
-            ( { model | submissions = newSubmission :: model.submissions }, fetchTime )
+            ( { model | submissions = newSubmission :: model.submissions }
+            , saveSubmissionAsJson submissionJson
+            )
 
         ReceiveTime timeString ->
             ( { model | date = timeString }, Cmd.none )
@@ -97,21 +115,63 @@ update msg model =
             in
             ( { model | submissions = updatedSubmissions }, Cmd.none )
 
+        FetchSubmissions ->
+            ( model, fetchSubmissions )
+
+        FetchedSubmissions result ->
+            case result of
+                Ok submissions ->
+                    ( { model | submissions = submissions }, Cmd.none )
+
+                Err _ ->
+                    ( { model | submissions = [] }, Cmd.none )
+
+        SubmissionSaved result ->
+            case result of
+                Ok _ ->
+                    ( model, fetchTime )
+
+                Err _ ->
+                    ( model, Cmd.none )
+
+encodeSubmission : Submission -> Encode.Value
+encodeSubmission submission =
+    Encode.object
+        [ ( "name", Encode.string submission.name )
+        , ( "link", Encode.string submission.link )
+        , ( "notes", Encode.string submission.notes )
+        , ( "date", Encode.string submission.date )
+        , ( "grade", Encode.string submission.grade )
+        ]
+
+saveSubmissionAsJson : Encode.Value -> Cmd Msg
+saveSubmissionAsJson submissionJson =
+    Http.post
+        { url = "/submit"
+        , body = Http.jsonBody submissionJson
+        , expect = Http.expectWhatever SubmissionSaved
+        }
 -- FETCH TIME
 
 fetchTime : Cmd Msg
 fetchTime =
     Task.perform (posixToMillis >> String.fromInt >> ReceiveTime) Time.now
 
--- VIEW
+fetchSubmissions : Cmd Msg
+fetchSubmissions =
+    Http.get
+        { url = "/submissions"
+        , expect = Http.expectJson FetchedSubmissions (Decode.list decodeSubmission)
+        }
 
-view : Model -> Html Msg
-view model =
-    if model.showAdminPage then
-        adminView model
-    else
-        userView model
-
+decodeSubmission : Decode.Decoder Submission
+decodeSubmission =
+    Decode.map5 Submission
+        (Decode.field "name" Decode.string)
+        (Decode.field "link" Decode.string)
+        (Decode.field "notes" Decode.string)
+        (Decode.field "date" Decode.string)
+        (Decode.field "grade" Decode.string)
 userView : Model -> Html Msg
 userView model =
     div [ class "flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4" ]
@@ -126,6 +186,7 @@ userView model =
             ]
         , button [ class "fixed bottom-4 right-4 bg-gray-800 text-white px-4 py-2 rounded", onClick ToggleAdminPage ] [ text "Admin" ]
         ]
+
 
 adminView : Model -> Html Msg
 adminView model =
@@ -147,15 +208,24 @@ adminView model =
                 )
           else
             text ""
+        , button [ class "fixed bottom-4 right-4 bg-gray-800 text-white px-4 py-2 rounded", onClick FetchSubmissions ] [ text "Refresh" ]
         ]
 
--- SUBSCRIPTIONS
+view : Model -> Html Msg
+view model =
+    if model.showAdminPage then
+        adminView model
+    else
+        userView model
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
 
--- MAIN
-
 main =
-    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
